a9f038b21399685104012ba2ef41b43fb87a0288
zul-adn <zbojetx@gmail.com>
2021-10-19 04:08:18 +0700

    change checkpoint

--- models/users.model.js
+++ models/users.model.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, Sequelize) => {
         username: {
             type: Sequelize.STRING,
         },
+        name: {
+            type: Sequelize.STRING,
+        },
         password: {
             type: Sequelize.STRING,
         },
